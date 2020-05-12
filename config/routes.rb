aa666a96b7048b9dd21169f36d19184df7bc0edd
David Sellam <dvdsellam@gmail.com>
2020-05-12 10:30:12 +0300

    Beyond CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+
+    resources :reviews, only: [:new, :create]
+
+    collection do
+      get :top #  /restaurants/top
+    end
+
+    member do
+      get :chef # /restaurants/:id/chef
+    end
+  end
+
+  resources :reviews, only: [ :destroy ]
 end
+
